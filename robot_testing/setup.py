315ab351139c040190563f7113088304b5c749fb
MENG2025-26 <st20231528@outlook.cardiffmet.ac.uk>
2026-04-07 12:22:37 +0100

    change the robot_testing file from a cmake to a python file

---
+++ robot_testing/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'robot_testing'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='cam',
+    maintainer_email='st20231528@outlook.cardiffmet.ac.uk',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+                'encoder_node = robot_testing.encoder_node_three:main',
+        ],
+    },
+)
